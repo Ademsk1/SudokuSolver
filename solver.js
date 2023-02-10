25df758eb204b7341e8317ccdb807069790fb03a
Ademsk1 <adam.illoul24@gmail.com>
2023-02-10 00:13:43 +0000

    obvious pairs now working

--- solver.js
+++ solver.js
@@ -1,5 +1,3 @@
-const { objectExpression } = require("@babel/types")
-
 function getRow(board,i) {
     return board[i]
 }
@@ -41,26 +39,50 @@ function getBox(board,i,j) {
 function removeEmpties(array) {
     return array.filter(el=> typeof el==='number' && el!==0)
 }
+
+function pairingsEliminatePossibility(board,type,value1,value2,crd1,crd2) {
+    const coordinates = findEmptyCellCoordinates(board,crd1[0],crd1[1],type)
+    const coi = coordinates.filter(coordinate=> { //coordinates of interest
+        return coordinate.toString()!==crd1.toString() && coordinate.toString()!==crd2.toString()
+    })
+    let cellValue
+    coi.forEach(crd=> {
+        cellValue = board[crd[0]][crd[1]]
+        if (cellValue.includes(value1)) {
+            cellValue.splice(cellValue.indexOf(value1),1)
+            board[crd[0]][crd[1]] = cellValue
+            //console.log(`${value1} eliminated at ${crd[0]},${crd[1]}`)
+        }
+        if (cellValue.includes(value2)) {
+            cellValue.splice(cellValue.indexOf(value2),1)
+            board[crd[0]][crd[1]] = cellValue
+        }
+    }
+
+    )
+
+
+}
+
 function obviousPairs(board,i,j,type) {
     const coordinates = findEmptyCellCoordinates(board,i,j,type)
     const possibleValues = coordinates.map((crd,crd_i)=> [board[crd[0]][crd[1]],crd]) 
-    const possiblePairs = possibleValues.filter(values=>values[0].length===2).map(el=> el[0]=`${el[0]}`)
+    const possiblePairs = possibleValues.filter(values=>values[0].length===2).map(el=>{
+        return [`${el[0]}`,el[1]]
+    })
     const matchingCells = []
     let value1,value2
     for (let l=0;l<possiblePairs.length;l++) {
         for (let k=l+1;k<possiblePairs.length;k++) {
             if (possiblePairs[l][0]===possiblePairs[k][0]) {
-                value1 = parseInt(possiblePairs[l][0][1]) //get the value from the string
-                value2 = parseInt(possiblePairs[l][0][3])
+                value1 = parseInt(possiblePairs[l][0][0]) //get the value from the string
+                value2 = parseInt(possiblePairs[l][0][2])
                 const [I1,J1] = possiblePairs[l][1]
                 const [I2,J2] = possiblePairs[k][1]
-                eliminatePossibility(value1,I1,J1,board,type)
-                eliminatePossibility(value2,I1,J1,board,type)
-                eliminatePossibility(value1,I2,J2,board,type)
-                eliminatePossibility(value2,I2,J2,board,type)
+                pairingsEliminatePossibility(board,type,value1,value2,[I1,J1],[I2,J2])
+                //console.log(`Found pairing ${board[I1][J1]} at ${I1},${J1} and ${I2},${J2} for ${type}`)
             }
         }
-
     }
 }
 function cellsPossibleValues(board,i,j) {
@@ -92,12 +114,11 @@ function eliminatePossibility(value,i,j,board,type) {
 }
 
 function singleValuedCells(board) {
-
     coordinates = findAllEmptyCells(board) //all coordinates of the board
     const possibleValues = coordinates.map(crd=> board[crd[0]][crd[1]])
     const singles = possibleValues.map((p,indx)=> [p,coordinates[indx]]).filter(el=>el[0].length===1)
     singles.forEach(el => {
-        const [value,[I,J]] = el
+        const [[value],[I,J]] = el
         board[I][J] = value
         eliminatePossibility(value,I,J,board,'all')
         })
@@ -111,7 +132,6 @@ function obviousSingles(board,i,j,type) {
     possibleValues.forEach((possibles,crdCounter)=> {
         possibles.forEach(possible=> {
             `${possible}` in valueCounter? valueCounter[`${possible}`][0]++:valueCounter[`${possible}`]=[1,coordinates[crdCounter]]
-
         })
     })
     const singularCells = Object.keys(valueCounter).reduce((acc,key)=> {
@@ -122,8 +142,6 @@ function obviousSingles(board,i,j,type) {
     },[])
     return singularCells
 }
-
-
 
 function findEmptyCellCoordinates(board,i,j,type) {
     if (type==='all') {
@@ -162,22 +180,29 @@ function sudokuSolver(board) {
     let emptyCellIndexes
     let unsolved = true
     let I,J
+    const oldBoard = [...board]
     emptyCellIndexes = findAllEmptyCells(board)
     for (let [i,j] of emptyCellIndexes) {
-        board[i][j]=cellsPossibleValues(board,i,j)
+        board[i][j]=cellsPossibleValues(board,i,j) //find all the possible values for each empty cell. 
         if (typeof board[i][j]==='number') {
-            eliminatePossibility(board[i][j],i,j,board,'all')
+            eliminatePossibility(board[i][j],i,j,board,'all') //fill in, if there can only be 1. 
         }
     }
-    
     while (unsolved) {
         emptyCellIndexes = findAllEmptyCells(board)
-        singleValuedCells(board)
+
+        singleValuedCells(board) //where only 1 value is possible in a cell, it is filled. (similar to cellsPossibleValues)
         for (let k=0;k<9;k++) {
+            singleValuedCells(board)
             I = Math.floor(k/3)*3
             J = (k % 3)*3
-            const boxSols = obviousSingles(board,I,J,'box')
-            const rowSols = obviousSingles(board,k,k,'row')
+            //console.log(getBox(board,6,3)[6])
+            //console.log(getBox(board,6,3)[8])
+            obviousPairs(board,k,k,'row')
+            obviousPairs(board,k,k,'column')
+            obviousPairs(board,I,J,'box')
+            const boxSols = obviousSingles(board,I,J,'box') //find a cell where within a box/row/col a potential value can only exist 
+            const rowSols = obviousSingles(board,k,k,'row') 
             const colSols =  obviousSingles(board,k,k,'column')
             for (let [value,[crdI,crdJ]] of [...rowSols,...colSols,...boxSols]) {
                 if (typeof board[crdI][crdJ]!=='number') {
@@ -186,11 +211,15 @@ function sudokuSolver(board) {
                 }
             }
         }
+        //console.log(board)
+        //console.log('------------')
+        
         if (emptyCellIndexes.length===0) {
             console.log(`dT = ${new Date() - startTime}`)
             unsolved=false
         }
     }
+    console.log(board)
     return board
 
 
@@ -198,15 +227,7 @@ function sudokuSolver(board) {
 
 }
 
-// medium = [[1,5,"","",9,"","","",2],
-//         ["","","",2,1,"",7,4,""],
-//         ["","","",7,5,3,"",9,""],
-//         ["",6,"",1,"","","","",9],
-//         [4,"","","","","",3,"",8],
-//         [9,"","","",7,"",5,"",1],
-//         [6,"","",5,3,"","","",""],
-//         ["","","","",2,"",6,1,""],
-//         ["",3,1,"","","","","",""]]
+
 const medium = [[1,5,0,0,9,0,0,0,2],
 [0,0,0,2,1,0,7,4,0],
 [0,0,0,7,5,3,0,9,0],
@@ -225,12 +246,23 @@ const evil = [[5,0,0,0,0,0,1,0,2],
               [0,0,3,6,0,0,0,1,0],
               [0,6,0,0,3,1,0,8,0],
               [2,0,9,0,0,0,0,0,6]]
-sudokuSolver(medium)
-
+//sudokuSolver(medium)
+const hard=[[0,0,7,0,0,3,5,4,0],
+            [0,0,0,2,0,0,0,0,0],
+            [0,3,0,0,8,4,0,7,2],
+            [0,9,0,0,0,0,0,0,7],
+            [0,1,0,7,0,0,0,0,0],
+            [7,0,0,1,0,0,2,8,0],
+            [1,0,5,0,0,9,0,0,0],
+            [9,6,0,0,0,8,0,0,5],
+            [3,0,0,0,6,0,9,0,0]]
+sudokuSolver(hard)
 
 module.exports = {
     cellsPossibleValues,
     eliminatePossibility,
     obviousSingles,
     sudokuSolver,
+    obviousPairs,
+    getBoxCoordinates
 }
